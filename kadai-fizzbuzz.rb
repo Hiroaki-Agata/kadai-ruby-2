db2f323b219b85bcb39f9067cb61b9275603b15f
Hiroaki Agata <el7273le@gmail.com>
2021-10-29 15:31:01 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,26 @@
+def fizzbuzz(num)
+    if num % 3 == 0
+        return "Fizz"
+    elsif num % 5 == 0
+        return "Buzz"
+    elsif num % 3 == 0 && num % 5 == 0
+        return "FizzBuzz"
+    else 
+        return num
+    end
+end
+
+num_max = 100
+num = 1
+
+while num <= num_max
+    puts fizzbuzz(num)  
+    num += 1
+end
+
+
+
+
+
+
+
